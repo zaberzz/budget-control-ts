cf6d538c7c554e23deb8c7c7283d0b6bf61a8a88
Saba <saba.sadegh67@gmail.com>
2023-04-09 14:06:50 +0430

    define variables

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-co
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toform = document.querySelector("#toform") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+});
