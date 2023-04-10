40be55174a24036ebf8d549dcfa21359e7e0409d
Saba <saba.sadegh67@gmail.com>
2023-04-10 18:35:04 +0430

    render element

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import { invoice } from "./classes/invoice.js";
+import { ListTEmplate } from "./classes/ListTemplate.js";
 import { payments } from "./classes/payments.js";
 import { hasformatter } from "./interfaces/HasFormatter.js";
 // const invOne = new invoice();
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
-const toform = document.querySelector("#toform") as HTMLInputElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
-
+const ul = document.querySelector("ul")!;
+const list = new ListTEmplate(ul);
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   let doc: hasformatter;
   if (type.value === "invoice") {
-    doc = new invoice(toform.value, details.value, amount.valueAsNumber);
+    doc = new invoice(tofrom.value, details.value, amount.valueAsNumber);
   } else {
-    doc = new payments(toform.value, details.value, amount.valueAsNumber);
+    doc = new payments(tofrom.value, details.value, amount.valueAsNumber);
   }
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
